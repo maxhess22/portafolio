77d3befe38608879629ac9c755eb886e0497ec19
maxhess22  email = ma.hessalvarez@duocuc.cl [credential]  helper = cache <max@LAPTOP-6MO6UKJU.localdomain>
2022-09-08 02:47:12 -0300

    proyecto calculadora

---
+++ Js/practico.js
@@ -0,0 +1,32 @@
+let disminuir = document.querySelector("#btn-disminuir");
+let resetear = document.querySelector("#btn-resetear");
+let incrementar = document.querySelector("#btn-incrementar");
+let h1 = document.querySelector("#resultado")
+let resultado = 0;
+
+incrementar.addEventListener("click", incrementarNumero);
+disminuir.addEventListener("click", disminuirNumero)
+resetear.addEventListener("click", limpiar)
+
+function limpiar(){
+    resultado = 0;
+    h1.textContent= `${resultado}`;
+}
+
+function incrementarNumero() {
+    resultado = resultado + 1;
+    h1.textContent= `${resultado}`;
+    
+    
+}
+
+function disminuirNumero() {
+    
+    if(resultado > 0){
+        resultado = resultado - 1;
+        h1.textContent= `${resultado}`; 
+    }    
+}
+
+
+
